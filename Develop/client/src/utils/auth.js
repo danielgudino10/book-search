e47b892d42b8c18532ebcf2f0ffa55e7086bf506
daniel gudino <dgudino459@gmail.com>
2023-12-26 01:41:10 -0800

    updates on the hints portion of the homework

--- Develop/client/src/utils/auth.js
+++ Develop/client/src/utils/auth.js
@@ -1,49 +1,49 @@
-// use this to decode a token and get the user's information out of it
+// Import the necessary packages
 import decode from 'jwt-decode';
+import { ApolloClient, InMemoryCache, createHttpLink, ApolloLink, from } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
-// create a new class to instantiate for a user
+// Create an Apollo HTTP link
+const httpLink = createHttpLink({
+  uri: 'YOUR_GRAPHQL_API_ENDPOINT', // Replace with your actual GraphQL API endpoint
+});
+
+// Create an Apollo auth link to set the JWT token in the request headers
+const authLink = setContext((_, { headers }) => {
+  // Get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // Set the token in the request headers
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// Create the Apollo Client instance
+const apolloClient = new ApolloClient({
+  link: from([authLink, httpLink]),
+  cache: new InMemoryCache(),
+});
+
+// Modify the AuthService class to work with GraphQL and JWT
 class AuthService {
-  // get user data
-  getProfile() {
-    return decode(this.getToken());
-  }
+  // ... (existing methods)
 
-  // check if user's logged in
-  loggedIn() {
-    // Checks if there is a saved token and it's still valid
-    const token = this.getToken();
-    return !!token && !this.isTokenExpired(token); // handwaiving here
-  }
-
-  // check if token is expired
-  isTokenExpired(token) {
+  // Example method to fetch user data from the GraphQL API
+  async fetchUserData() {
     try {
-      const decoded = decode(token);
-      if (decoded.exp < Date.now() / 1000) {
-        return true;
-      } else return false;
-    } catch (err) {
-      return false;
+      const userData = await apolloClient.query({
+        query: YOUR_USER_QUERY, // Replace with your actual user query
+      });
+      return userData;
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+      throw error;
     }
-  }
-
-  getToken() {
-    // Retrieves the user token from localStorage
-    return localStorage.getItem('id_token');
-  }
-
-  login(idToken) {
-    // Saves user token to localStorage
-    localStorage.setItem('id_token', idToken);
-    window.location.assign('/');
-  }
-
-  logout() {
-    // Clear user token and profile data from localStorage
-    localStorage.removeItem('id_token');
-    // this will reload the page and reset the state of the application
-    window.location.assign('/');
   }
 }
 
+// Export the modified AuthService instance
 export default new AuthService();
--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,21 +1,17 @@
 const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const typeDefs = require('./path-to-your-graphql-schema'); // Replace with the path to your GraphQL schema
+const resolvers = require('./path-to-your-resolvers'); // Replace with the path to your resolvers
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Create an instance of Apollo Server and apply it as middleware to the Express server
+const server = new ApolloServer({ typeDefs, resolvers });
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+server.applyMiddleware({ app });
 
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
 });
